45200ac0bd9fa9dfdec4c1465d6d1c59f55628cf
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-18 21:53:19 +0200

    First commit : Add Task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+#!/opt/homebrew/bin/node
+const fs = require('fs');
+
+// Get the file path from the command line argument
+const filePath = process.argv[2];
+
+// Read the content of the file in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
+  } else {
+    // Print the content of the file
+    console.log(data);
+  }
+});

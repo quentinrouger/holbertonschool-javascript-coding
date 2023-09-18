6b2a4b0f38c96ae7eee9d65dc21db15ba374d95c
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-18 22:45:25 +0200

    Add: Task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get the file path and string to write from command line arguments
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+// Write the content to the file in utf-8
+fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
+  } else {
+    console.log(`Content written to ${filePath}`);
+  }
+});

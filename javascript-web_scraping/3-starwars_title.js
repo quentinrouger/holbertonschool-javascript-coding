396c2408498a972a7ef4d0bd6a7ccc74515aacb5
quentinrouger <quentin_rouger@hotmail.fr>
2023-09-19 21:55:41 +0200

    Add: Task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-// Get the movie ID from the command line argument
 const movieId = process.argv[2];
 
-// Define the URL to the Star Wars API endpoint with the movie ID
 const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-// Perform a GET request to fetch movie data
 request.get(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error:', error.message);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: Status Code ${response.statusCode}`);
+    console.error(error);
   } else {
+    if (response.statusCode === 200) {
       const movieData = JSON.parse(body);
-      console.log(`Title: ${movieData.title}`);
+      console.log(movieData.title);
     }
+  }
 });

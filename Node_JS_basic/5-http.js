951342e76895da2f20fd671f2de6c35cca7c9495
quentinrouger <quentin_rouger@hotmail.fr>
2023-10-10 19:38:45 +0200

    Add: Task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -4,17 +4,29 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.end('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
+      const { totalStudents, fieldsCount } = await countStudents(process.argv[2]);
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${totalStudents}\n`);
+      for (const field in fieldsCount) {
+        if (field !== 'field') {
+          const fieldStudents = fieldsCount[field].length;
+          const firstNameList = fieldsCount[field].join(', ');
+          res.write(`Number of students in ${field}: ${fieldStudents}. List: ${firstNameList}\n`);
+        }
+      }
+      res.end();
     } catch (error) {
       res.end(error.message);
     }
+  } else {
+    res.end();
   }
-  res.end();
 });
+
 app.listen(1245);
 module.exports = app;

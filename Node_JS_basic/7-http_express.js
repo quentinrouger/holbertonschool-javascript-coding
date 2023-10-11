1591c659fad80980a3c92c4f2e33fb6161c97f93
quentinrouger <quentin_rouger@hotmail.fr>
2023-10-11 15:24:52 +0200

    Add: Task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,31 +1,24 @@
 const express = require('express');
-
-const app = express();
-const port = 1245;
-
-// Import the countStudents function from 3-read_file_async.js
 const countStudents = require('./3-read_file_async');
 
+const DB = process.argv[2];
+const port = 1245;
+const app = express();
+module.exports = app;
+
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('content-type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', async (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+app.get('/students', (req, res) => {
+  res.setHeader('content-type', 'text/plain');
   res.write('This is the list of our students\n');
-  try {
-    // Pass the name of the database as an argument to countStudents
-    const data = await countStudents('database.csv');
-    // Join the array elements into a single string
-    res.end(data.join('\n'));
-  } catch (error) {
+  countStudents(DB).then((result) => {
+    res.end(result.join('\n'));
+  }).catch((error) => {
     res.end(`${error.message}`);
-  }
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-
-module.exports = app;
+app.listen(port);
